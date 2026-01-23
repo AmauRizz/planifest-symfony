6c3450e991a780eb2db92746ef440bc45ab52b6d
amaury <amaury.mulcey.pro@gmail.com>
2026-01-23 20:41:15 +0100

    Configure tailwind

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import './assets/styles/reset.css'
 import './assets/styles/global.css'
+import './assets/styles/main.css'
 import App from './App.vue'
 import { setupInterceptors } from '@/api/interceptors.ts';
 
