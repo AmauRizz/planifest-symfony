e65094d5079f5c2dfb7c2dcf3fc018974cdb4365
amaury <amaury.mulcey.pro@gmail.com>
2026-01-23 20:50:26 +0100

    Add shadcn

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,16 +1,13 @@
-import path from 'node:path'
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
       vue(),
-      tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      tailwindcss(),
+      tsconfigPaths()
+  ]
 })
